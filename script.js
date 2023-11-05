99ec54a7b7748a66fed1fb5d23b046419c6b5fa7
AiNghiDiep1668 <146017268+AiNghiDiep1668@users.noreply.github.com>
2023-11-04 19:59:26 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// function ProfileImg(x) {
+//     //x.style.height = "64px";
+//     //x.style.width = "64px";
+//     const x = new Image(100, 200);
+//     x.src = "https://o.remove.bg/downloads/76795396-89c4-4a17-87d0-82e17803a98e/image-removebg-preview.png";
+//     document.body.appendChild(x);
+//   }
+  
+//   function DifImg(x) {
+//     //x.style.height = "32px";
+//     //x.style.width = "32px";
+//     const x = new Image(100, 200);
+//     x.src = "https://o.remove.bg/downloads/6c78bed8-69af-4927-89a4-200711c3b519/image-removebg-preview.png";
+//     document.body.appendChild(x);
+//   }
